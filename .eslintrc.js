27d5601712ce6a8e26f008c293d5c5e55847d29e
Koen Castermans <castermanskoen@msn.com>
2018-09-09 15:08:35 +0200

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      commonjs: true
+    },
+    globals: {
+      process: true,
+      __VERSION_NUMBER__: true,
+      __BUILD_IDENTIFIER__: true
+    },
+    plugins: ["compat", "prettier", "react"],
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parser: "babel-eslint",
+    parserOptions: {
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+        modules: true,
+        experimentalObjectRestSpread: true
+      }
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "compat/compat": "off",
+      "react/no-unknown-property": ["error", { ignore: ["class"] }],
+      "react/prop-types": "off",
+      "react/display-name": "off"
+    }
+  };
