defa482a188787cdd102f8e01d4fd0b7f4917541
Koen Castermans <castermanskoen@msn.com>
2018-11-11 17:01:25 +0100

    Add more styling options to inputButtom componetn

--- src/components/form/inputButton.js
+++ src/components/form/inputButton.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./inputButton.module.scss";
 
-const InputButton = ({ defaults, primary, fit }) => {
+const InputButton = ({ defaults, primary, fit, small, large, alt }) => {
   let buttonStyles = styles.button;
   if (primary) buttonStyles += ` ${styles.buttonPrimary}`;
   if (fit) buttonStyles += ` ${styles.buttonFit}`;
+  if (small) buttonStyles += ` ${styles.buttonSmall}`;
+  if (large) buttonStyles += ` ${styles.buttonLarge}`;
 
   return <input {...defaults} className={buttonStyles} />;
 };
