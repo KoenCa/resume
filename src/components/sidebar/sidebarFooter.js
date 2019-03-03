8a1fce942e688f29fdcdb8028157a732b3f23ba5
Koen Castermans <castermanskoen@msn.com>
2019-03-03 22:11:15 +0100

    Add links for email, GitHub and LinkedIn

--- src/components/sidebar/sidebarFooter.js
+++ src/components/sidebar/sidebarFooter.js
@@ -6,19 +6,29 @@ const SidebarFooter = () => (
   <footer className={styles.footer}>
     <ul className={styles.footerIcons}>
       <li className={styles.footerIcon}>
-        <a href="#" className="icon">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/koen-castermans-bb1959142/"
+          className="icon"
+        >
           <FontAwesomeIcon icon={["fab", "linkedin"]} />
           <span className="label">LinkedIn</span>
         </a>
       </li>
       <li className={styles.footerIcon}>
-        <a href="#" className="icon">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/KoenCa"
+          className="icon"
+        >
           <FontAwesomeIcon icon={["fab", "github"]} />
           <span className="label">Github</span>
         </a>
       </li>
       <li className={styles.footerIcon}>
-        <a href="#" className="icon fa-envelope">
+        <a href="mailto:castermanskoen@msn.com" className="icon fa-envelope">
           <FontAwesomeIcon icon="envelope" />
           <span className="label">Email</span>
         </a>
