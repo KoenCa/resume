fb8bcc95cd039ffdfccbcb361b18c17efefee157
Koen Castermans <castermanskoen@msn.com>
2023-10-09 21:08:13 +0200

    Remove pollyfill libraries
    They are not needed anymore since, because all modern browser now support the features.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,2 @@
 import "./src/sass/main.scss";
 import "./src/utils/font-awesome";
-import "./src/utils/smooth-scroll";
-
-export const onClientEntry = async () => {
-  if (typeof IntersectionObserver === `undefined`) {
-    await import(`intersection-observer`);
-  }
-};
